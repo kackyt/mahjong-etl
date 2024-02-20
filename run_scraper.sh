a004d7e178e7d2eca69b8dd0741354f79c84ebf4
kacky <t_kaki@nextappli.com>
2024-02-20 21:54:01 +0900

    feat: とある月のみscrapeするスクリプト

---
+++ run_scraper.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir logs
+gcloud storage cp gs://${BUCKET_NAME}/logs/${DATE}* ./logs --recursive -x '.*\.DS_Store'
+
+poetry run python main.py -L logs -O parquet
+
+gcloud storage rsync parquet gs://${BUCKET_NAME}/datasets/tenhou --recursive -x '.*\.DS_Store'
